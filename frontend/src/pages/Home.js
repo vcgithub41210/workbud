38b4f042fa444e569860bf692446d28d3e4cdc84
unknown <vinayc.11b.kmbvm@gmail.com>
2024-03-10 10:50:42 +0530

    connecting backend and frontend

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,24 @@
+import {useState, useEffect} from 'react';
+
 const Home = ()=>{
+    const [workouts,setWorkouts] = useState(null);
+    useEffect(()=>{
+        const fetchWorkouts = async () => {
+            const response = await fetch('/api/workouts');
+            const json = await response.json();
+            if(response.ok){
+                setWorkouts(json);
+            }
+        }
+        fetchWorkouts();
+    },[]);
     return(
         <div className="Home">
-            <h2>Home</h2>
+            <div className='workouts'>
+                {workouts && workouts.map((workout)=>{
+                    <p key={workout.id}>{workout.title}</p>
+                })}
+            </div>
         </div>
     )
 }
