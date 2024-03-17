f92d5095d8b537773e26744ec934422aa407d84d
unknown <vinayc.11b.kmbvm@gmail.com>
2024-03-18 00:25:06 +0530

    icons

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -19,7 +19,7 @@ const WorkoutDetails = ({ workout }) =>{
             <p><strong>Load (kg): </strong>{workout.load}</p>
             <p><strong>Reps: </strong>{workout.reps}</p>
             <p>{workout.createdAt}</p>
-            <span onClick={handleClick}>Delete</span>
+            <span className="material-symbols-outlined" onClick={handleClick}>delete</span>
         </div>
     )
 }
